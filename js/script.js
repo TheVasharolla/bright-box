ed70de7149478418484df7352174f6ad4f3e861a
Krisley <chrisleyonyeze@gmail.com>
2024-08-03 19:12:37 +0100

    final

---
+++ js/script.js
@@ -0,0 +1,5 @@
+// // Add any necessary JavaScript here. For example, handling the subscription form
+// document.querySelector('.subscribe form').addEventListener('submit', function(e) {
+//     e.preventDefault();
+//     alert('Subscribed successfully!');
+// });
